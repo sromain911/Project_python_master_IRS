df0e46d3e5ed84e6308a5d8a6f7bcc51d8c705de
sromain911 <162327908+sromain911@users.noreply.github.com>
2024-07-08 15:41:44 +0200

    Update main.py

--- program/main.py
+++ program/main.py
@@ -1,1 +1,19 @@
-print("hello")
+#PSL Modules
+
+#External modules
+#import customtkinter
+import tk
+#Custom modules
+import app
+
+# Main function (entry-point)
+def main():
+    #application nexw instance
+    new_app_instance =app.Application()
+
+    new_app_instance.launch_app()
+    new_app_instance.display_menu()
+
+#Main Guard
+if __name__ == "__main__":
+    main()
