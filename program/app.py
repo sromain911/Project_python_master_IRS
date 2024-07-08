e9c23922be919e1414032de81f42b492c90ce66a
sromain911 <162327908+sromain911@users.noreply.github.com>
2024-07-08 15:42:15 +0200

    Create app.py

---
+++ program/app.py
@@ -0,0 +1,16 @@
+""" Application Main Class
+
+"""
+
+class Application:
+    def __init__(self):
+        pass
+
+    def launch_app(self):
+        print("Loading...")
+
+    def display_menu(self):
+        print("Main Menu\n1 Start a new game\n2 Load Game\n3 Quit")
+
+    def quit_app(self):
+        sys.exit()
